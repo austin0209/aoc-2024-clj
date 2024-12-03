c6290e6e582252dec44fe7ec0dd2c37a240ed25b
Austin Aurelio <austin@delveam.com>
2024-12-03 13:49:47 -0600

    Solve day 2 part 2

--- src/aoc_2024_clj/day02.clj
+++ src/aoc_2024_clj/day02.clj
@@ -15,7 +15,7 @@
         (str/split-lines i)
         (map #(read-string (str "[" % "]")) i)))
 
-(defn is-safe [levels]
+(defn is-safe-pt1? [levels]
   (let [diffs (->> (partition 2 1 levels)
                    (map #(- (first %) (second %))))
         sign (if (zero? (first diffs))
@@ -27,8 +27,22 @@
          (count)
          (= (count diffs)))))
 
+(defn is-safe-pt2? [levels]
+  (if (is-safe-pt1? levels)
+    true
+    (loop [i 0]
+      (if (= i (count levels))
+        false
+        (let [removed (keep-indexed #(when (not= i %1) %2) levels)]
+          (if (is-safe-pt1? removed)
+            true
+            (recur (+ 1 i))))))))
+
 ;; Part 1
-(count (filter true? (map is-safe (parse-input input))))
+(count (filter true? (map is-safe-pt1? (parse-input input))))
+
+;; Part 2
+(count (filter true? (map is-safe-pt2? (parse-input input))))
 
 (def input "1 3 5 6 8 9 12 9
 66 67 70 72 73 74 75 75
