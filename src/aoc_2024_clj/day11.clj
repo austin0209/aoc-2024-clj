183b8320feed3632e51c562192333b33a4dbaaa2
Austin Aurelio <austin@delveam.com>
2024-12-13 03:55:51 -0600

    Solve day 11 part 2
    Full credit to reddit for the "inspiraition". The missing piece was
    remembering that we only care about the count. This sped up the entire
    function since it didn't have to generate so many values and iterate
    over them. Other than that, I think I had the right idea barring
    overcomplicating the memoization in my previous implementations.

--- src/aoc_2024_clj/day11.clj
+++ src/aoc_2024_clj/day11.clj
@@ -13,17 +13,34 @@
 
 (defn process-stone [stone]
   (cond
-    (= 0 stone) 1
+    (= 0 stone) [1]
     (even? (count (str stone))) (split-stone stone)
-    :else (* 2024 stone)))
+    :else [(* 2024 stone)]))
 
 (defn blink [stones]
-  (->> (map process-stone stones)
-       (flatten)
-       (vec)))
+  (mapcat process-stone stones))
+
+(def nth-count
+  (memoize
+    (fn [stone n]
+      (cond
+        (= n 0) 1
+        (= 0 stone) (nth-count 1 (dec n))
+        (even? (count (str stone)))
+        (let [split (split-stone stone)]
+          (+ (nth-count (first split) (dec n))
+             (nth-count (second split) (dec n))))
+        :else (nth-count (* 2024 stone) (dec n))))))
 
 (defn solve-p1 [input]
   (count (last (take 26 (iterate blink (parse-input input))))))
 
+(defn solve-p2 [input]
+  (->> (parse-input input)
+       (map #(nth-count % 75))
+       (apply +)))
+
 (comment
-  (solve-p1 in/input))
+  (solve-p1 in/input)
+  (solve-p2 in/input)
+  (nth-count 125 6))
