3fc3a3afe87c9e19611eab0b6e32ea4b7fc8dfba
Austin Aurelio <austin@delveam.com>
2024-12-13 03:20:16 -0600

    Give up on day 11 part 2 for now
    My solution was wayy to overcomplicated and I started spiralling. After
    looking at other solutions, I realized I'm not gonna get anywhere with
    my current solution.

--- src/aoc_2024_clj/day11.clj
+++ src/aoc_2024_clj/day11.clj
@@ -19,54 +19,11 @@
 
 (defn blink [stones]
   (->> (map process-stone stones)
-       (flatten)))
-
-(defn blink-windowed [window-size stones]
-  (->> (partition window-size window-size '() stones)
-       (pmap blink)
-       (flatten)))
-
-(def blink-cache (atom {}))
-
-(defn nth-stone-blink [stone n]
-  (if (contains? @blink-cache [stone n])
-    (get @blink-cache [stone n])
-    (let [ns-cached (->> (keys @blink-cache)
-                         (filter #(= stone (first %)))
-                         (map second))
-          last-cached-n (if (empty? ns-cached) 0 (apply max ns-cached))
-          cached-stones (get @blink-cache [stone last-cached-n] [stone])]
-      (loop [curr-stones cached-stones
-             blinks-done last-cached-n]
-        (if (= blinks-done n)
-          curr-stones
-          (let [next-blink (blink curr-stones)
-                blinks-done (inc blinks-done)]
-            (swap! blink-cache assoc [stone blinks-done] next-blink)
-            (recur next-blink blinks-done)))))))
-
-(comment
-  (time (last (nth-stone-blink 125 75)))
-  @blink-cache
-  (reset! blink-cache {}))
+       (flatten)
+       (vec)))
 
 (defn solve-p1 [input]
   (count (last (take 26 (iterate blink (parse-input input))))))
 
-(defn solve-p2 [input]
-  (let [stones (parse-input input)]
-    (loop [curr-stones stones
-           n 1]
-      (println n)
-      (if (= n 26)
-        (count curr-stones)
-        (let [next-blink (->> (mapv #(nth-stone-blink % n) curr-stones)
-                              (flatten))]
-          (recur next-blink (inc n)))))))
-
 (comment
-  (solve-p1 in/input)
-  (solve-p2 in/input)
-  (time (count (last (take 26 (iterate (partial blink-windowed 100) (parse-input in/input)))))))
-
-;; Need to memoize against stone # and distance taken!
+  (solve-p1 in/input))
